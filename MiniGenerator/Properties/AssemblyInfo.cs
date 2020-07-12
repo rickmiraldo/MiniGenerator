2f52085e2f3dbe9c952db5077e9df67d329f52f2
Henrique Miraldo <rickmiraldo@gmail.com>
2020-07-12 11:36:47 +0100

    Change default border to 4

--- MiniGenerator/Properties/AssemblyInfo.cs
+++ MiniGenerator/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.20170.2216")]
-[assembly: AssemblyFileVersion("1.0.20170.2216")]
+[assembly: AssemblyVersion("1.1.20182.1237")]
+[assembly: AssemblyFileVersion("1.1.20182.1237")]
